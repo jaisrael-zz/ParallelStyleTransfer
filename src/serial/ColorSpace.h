d7f9c9c6306d093be2f44b83c678d181ccfeae62
Michael <michaeljkamm@gmail.com>
2014-05-04 22:35:27 -0400

    YIQ implemented correctly. Results are pretty

--- src/serial/ColorSpace.h
+++ src/serial/ColorSpace.h
@@ -3,6 +3,7 @@
 
 #include <opencv2/core/core.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
+#include "math.h"
 
 using namespace cv;
 
@@ -10,10 +11,13 @@ class ColorSpace
 {
 public:
   void RGB_to_YIQ(const uchar blue, const uchar green, const uchar red,
-                  uchar& Y, uchar& I, uchar& Q);
+                  double& Y, double& I, double& Q);
   
-  void YIQ_to_RGB(const uchar Y, const uchar I, const uchar Q,
+  void YIQ_to_RGB(const double Y, const double I, const double Q,
                   uchar& blue, uchar& green, uchar& red);
+
+  void find_mean_stddev(const cv::Mat& Y, double& mean, double& stddev);
+  void luminance_remap(const cv::Mat& YA, const cv::Mat& YB, cv::Mat& YA_remapped);
 
   void RGB_to_YIQ(const cv::Mat& RGB, cv::Mat& YIQ);
   void YIQ_to_RGB(const cv::Mat& YIQ, cv::Mat& RGB);
@@ -21,26 +25,80 @@ public:
 
 };
 
-void ColorSpace::RGB_to_YIQ(const uchar blue, const uchar green, const uchar red,
-                            uchar& Y, uchar& I, uchar& Q)
+void ColorSpace::find_mean_stddev(const Mat& Y, double& mean, double& stddev)
 {
-  int iY = (int) (0.299*red + 0.587*green + 0.114*blue);
-  int iI = (int) (0.596*red - 0.275*green - 0.321*blue);
-  int iQ = (int) (0.212*red - 0.523*green + 0.311*blue);
+  int n = Y.rows*Y.cols;
+  mean = 0.0;
+  for (int r = 0; r < Y.rows; r++)
+  {
+    const uchar* y_row_ptr = Y.ptr<uchar>(r);
+    for (int c = 0; c < Y.cols; c++)
+    {
+      mean += *y_row_ptr;
+      y_row_ptr++;
+    }
+  }   
+  mean = mean / n;
 
-  if (iY > 255) iY = 255; if (iY < 0) iY = 0;
-  if (iI > 255) iI = 255; if (iI < 0) iI = 0;
-  if (iQ > 255) iQ = 255; if (iQ < 0) iQ = 0;
+  double sum_squares = 0;
 
-  Y = iY; I = iI; Q = iQ;
+  for (int r = 0; r < Y.rows; r++)
+  {
+    const uchar* y_row_ptr = Y.ptr<uchar>(r);
+    for (int c = 0; c < Y.cols; c++)
+    {
+      sum_squares += pow(*y_row_ptr - mean,2);
+      y_row_ptr++;
+    }
+  }
+  
+  stddev = sum_squares / n;
+  stddev = sqrt(stddev);
 }
 
-void ColorSpace::YIQ_to_RGB(const uchar Y, const uchar I, const uchar Q,
+void ColorSpace::luminance_remap(const Mat& YA, const Mat& YB, Mat& YA_remapped)
+{
+  double stddev_b, stddev_a, mean_b, mean_a;
+  YA_remapped.create(YA.rows, YA.cols, YA.type());
+  //find mean for each
+  find_mean_stddev(YA, mean_a, stddev_a);
+  find_mean_stddev(YB, mean_b, stddev_b);
+
+  for (int r = 0; r < YA.rows; r++)
+  {
+    const uchar* y_row_ptr = YA.ptr<uchar>(r);
+    uchar* yrm_row_ptr = YA_remapped.ptr<uchar>(r);
+    for (int c = 0; c < YA.cols; c++)
+    {
+
+      int result = (int) ((stddev_b / stddev_a)*(*y_row_ptr - mean_a) + mean_b);
+      if (result > 255) result = 255; if (result < 0) result = 0;
+      *yrm_row_ptr = result;
+
+      y_row_ptr++;
+      yrm_row_ptr++;
+    }
+  }
+}
+
+void ColorSpace::RGB_to_YIQ(const uchar blue, const uchar green, const uchar red,
+                            double& Y, double& I, double& Q)
+{
+  int iY = (int) (0.299*(double)red + 0.587*(double)green + 0.114*(double)blue);
+  I = (0.595716*(double)red - 0.274453*(double)green - 0.321263*(double)blue);
+  Q= (0.211456*(double)red - 0.522591*(double)green + 0.311135*(double)blue);
+
+  if (iY > 255) iY = 255; if (iY < 0) iY = 0;
+
+  Y = iY;
+}
+
+void ColorSpace::YIQ_to_RGB(const double Y, const double I, const double Q,
                             uchar& blue, uchar& green, uchar& red)
 {
-  int i_red   = (int) (Y + 0.95568806036116*I + 0.61985809445637*Q);
-  int i_green = (int) (Y - 0.27158179694406*I - 0.64687381613840*Q);
-  int i_blue  = (int) (Y - 1.10817732668266*I + 1.70506455991918*Q);
+  int i_red   = (int) (Y + 0.9563*I + 0.621*Q);
+  int i_green = (int) (Y - 0.2721*I - 0.6474*Q);
+  int i_blue  = (int) (Y - 1.1070*I + 1.7046*Q);
 
   if (i_red > 255)   i_red = 255;   if (i_red < 0)   i_red = 0;
   if (i_green > 255) i_green = 255; if (i_green < 0) i_green = 0;
@@ -52,11 +110,11 @@ void ColorSpace::YIQ_to_RGB(const uchar Y, const uchar I, const uchar Q,
 
 void ColorSpace::RGB_to_YIQ(const cv::Mat& RGB, cv::Mat& YIQ)
 {
-  YIQ.create(RGB.rows, RGB.cols, RGB.type());
+  YIQ.create(RGB.rows, RGB.cols, CV_64FC3);
   for (int r = 0; r < RGB.rows; r++)
   {
     const Vec3b* rgb_row_ptr = RGB.ptr<Vec3b>(r);
-    Vec3b* yiq_row_ptr = YIQ.ptr<Vec3b>(r);
+    Vec3d* yiq_row_ptr = YIQ.ptr<Vec3d>(r);
     for (int c = 0; c < RGB.cols; c++)
     {
       RGB_to_YIQ((*rgb_row_ptr)[0], (*rgb_row_ptr)[1], (*rgb_row_ptr)[2],
@@ -72,10 +130,10 @@ void ColorSpace::RGB_to_YIQ(const cv::Mat& RGB, cv::Mat& YIQ)
 void ColorSpace::YIQ_to_RGB(const cv::Mat& YIQ, cv::Mat& RGB)
 {
 
-  RGB.create(YIQ.rows, YIQ.cols, YIQ.type());
+  RGB.create(YIQ.rows, YIQ.cols, CV_8UC3);
   for (int r = 0; r < RGB.rows; r++)
   {
-    const Vec3b* yiq_row_ptr = YIQ.ptr<Vec3b>(r);
+    const Vec3d* yiq_row_ptr = YIQ.ptr<Vec3d>(r);
     Vec3b* rgb_row_ptr = RGB.ptr<Vec3b>(r);
     for (int c = 0; c < RGB.cols; c++)
     {
@@ -93,15 +151,15 @@ void ColorSpace::YIQ_to_RGB(const cv::Mat& Y, const cv::Mat& YIQ, cv::Mat& RGB)
 {
 
   
-  RGB.create(YIQ.rows, YIQ.cols, YIQ.type());
+  RGB.create(YIQ.rows, YIQ.cols, CV_8UC3);
   assert(Y.rows == YIQ.rows);
   assert(Y.cols == YIQ.cols);
   
 
   for (int r = 0; r < RGB.rows; r++)
   {
-    const Vec3b* yiq_row_ptr = YIQ.ptr<Vec3b>(r);
-    const uchar* y_row_ptr = Y.ptr<uchar>(r);
+    const Vec3d* yiq_row_ptr = YIQ.ptr<Vec3d>(r);
+    const double* y_row_ptr = Y.ptr<double>(r);
     Vec3b* rgb_row_ptr = RGB.ptr<Vec3b>(r);
     for (int c = 0; c < RGB.cols; c++)
     {
--- src/serial/main.cpp
+++ src/serial/main.cpp
@@ -19,23 +19,22 @@ int main(int argc, char** argv)
     return -1;
   }
 
-  //IMREAD IMAGES MUST BE GRAYSCALE
   Mat A = imread(argv[1], -1);
   Mat Ap = imread(argv[2], -1);
   Mat B = imread(argv[3], -1);
-  //cvtColor(A_8u1, A_8u1, CV_BGR2GRAY);
-  //cvtColor(B_8u1, B_8u1, CV_BGR2GRAY);
-  //cvtColor(Ap_8u1, Ap_8u1, CV_BGR2GRAY);
   
   ColorSpace cs;
   Mat A_YIQ, Ap_YIQ, B_YIQ;
   Mat A_RGB;
+  //RGB TO YIQ
   cs.RGB_to_YIQ(A, A_YIQ);
   cs.RGB_to_YIQ(Ap, Ap_YIQ);
   cs.RGB_to_YIQ(B, B_YIQ);
   cs.YIQ_to_RGB(A_YIQ, A_RGB);
-  namedWindow("A_RGB", CV_WINDOW_AUTOSIZE);
-  imshow("A_RGB", A_RGB); 
+  namedWindow("A_RGB_orig", CV_WINDOW_AUTOSIZE);
+  imshow("A_RGB_orig", A); 
+  namedWindow("A_RGB_remapped", CV_WINDOW_AUTOSIZE);
+  imshow("A_RGB_remapped", A_RGB); 
 
 
   Mat A_YIQ_split[3];
@@ -46,24 +45,27 @@ int main(int argc, char** argv)
   split(Ap_YIQ, Ap_YIQ_split);
   split(B_YIQ, B_YIQ_split);
   int num_levels = 5;
-  
-  ImageAnalogy IA(2, 5, num_levels);
 
-  namedWindow("Ay", CV_WINDOW_AUTOSIZE);
-  imshow("Ay", A_YIQ_split[0]); 
-  namedWindow("APy", CV_WINDOW_AUTOSIZE);
-  imshow("APy", Ap_YIQ_split[0]); 
-  namedWindow("B", CV_WINDOW_AUTOSIZE);
-  imshow("B", B_YIQ_split[0]); 
+  Mat A_YIQ_8u, Ap_YIQ_8u, B_YIQ_8u;
+  A_YIQ_split[0].convertTo(A_YIQ_8u, CV_8UC1); 
+  Ap_YIQ_split[0].convertTo(Ap_YIQ_8u, CV_8UC1); 
+  B_YIQ_split[0].convertTo(B_YIQ_8u, CV_8UC1); 
+  Mat A_Y_RM = A_YIQ_8u; 
+  Mat Ap_Y_RM = Ap_YIQ_8u; 
+  ImageAnalogy IA(1, 5, num_levels);
+  cs.luminance_remap(A_YIQ_8u, B_YIQ_8u, A_Y_RM);
+  cs.luminance_remap(Ap_YIQ_8u, B_YIQ_8u, Ap_Y_RM);
   waitKey(0);
   
 
   Mat BP_Y, BP;
-  IA.create_image_analogy(A_YIQ_split[0], Ap_YIQ_split[0], B_YIQ_split[0], BP_Y);
+  IA.create_image_analogy(A_Y_RM, Ap_Y_RM, B_YIQ_8u, BP_Y);
   //IA.create_image_analogy(A_8u1, Ap_8u1, B_8u1, BP);
   //imwrite(
 
-  cs.YIQ_to_RGB(BP_Y, B_YIQ, BP);
+  Mat BP_Y_64f;
+  BP_Y.convertTo(BP_Y_64f, CV_64FC1); 
+  cs.YIQ_to_RGB(BP_Y_64f, B_YIQ, BP);
  
   std::stringstream ss;
   ss << num_levels << ".jpg";
@@ -71,13 +73,11 @@ int main(int argc, char** argv)
   std::string bn = std::string(argv[3]);
   int pos = bn.find(bn, '.');
   bn = bn.substr(7, pos - 7);
-  filename = std::string("Results/flann_coh_YIQ") + bn + std::string("-l") + ss.str();
+  filename = std::string("Results/flann_coh1_CORRECTYIQ") + bn + std::string("-l") + ss.str();
   //std::cout << filename << std::endl; 
   imwrite(filename, BP);
 
 
-  namedWindow("BP_Y", CV_WINDOW_AUTOSIZE);
-  imshow("BP_Y", BP_Y);
   namedWindow("BP", CV_WINDOW_AUTOSIZE);
   imshow("BP", BP);
   namedWindow("A", CV_WINDOW_AUTOSIZE);
